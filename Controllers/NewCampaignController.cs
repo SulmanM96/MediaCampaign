03cfc18110af9a96b1487bddf391d0d0657ea980
Sulman <sulman.malik@hotmail.co.uk>
2019-06-13 15:12:44 +0100

    Initial commit

---
+++ Controllers/NewCampaignController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MediaCampaign.Models;
+
+namespace MediaCampaign.Models
+{
+    public class NewCampaignController : Controller
+    {
+
+        List<Campaign> Table = new List<Campaign>();
+        Campaign sul = new Campaign();
+        Campaign sul2 = new Campaign();
+
+     
+        // GET: Companies in a list
+        public ActionResult Index()
+        {
+            sul.CampaignName1 = "Petersburg";
+            sul.CampaignType1 = "TV";
+            sul.CompanyName1 = "Tesco";
+            sul.EndDate1 = 2018;
+            sul.StartDate1 = 2020;
+
+            sul2.CompanyName1 = "";
+            return View(sul);
+        }
+
+        // GET: NewCampaign/Details/5
+        public ActionResult Details(int id)//view db
+        {
+            return View();
+        }
+
+        // GET: NewCampaign/Create
+        public ActionResult Create() //new entries
+        {
+            return View();
+        }
+
+        public ActionResult Edit() //new entries
+        {
+            sul2.CompanyName1 = "";
+            return View(sul2.CampaignName1);
+        }
+
+       
+    }
+}
---
+++ Models/Campaign.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MediaCampaign.Models
+{
+    public class Campaign
+    {
+        private string CompanyName;
+        private string CampaignName;
+        private string CampaignType;
+        private int StartDate;
+        private int EndDate;
+
+        [DisplayName("Company Name")]
+        public string CompanyName1 { get => CompanyName; set => CompanyName = value; }
+        [DisplayName("Campaign Name")]
+        public string CampaignName1 { get => CampaignName; set => CampaignName = value; }
+        [DisplayName("Campaign Type")]
+        public string CampaignType1 { get => CampaignType; set => CampaignType = value; }
+        [DisplayName("Start Date")]
+        public int StartDate1 { get => StartDate; set => StartDate = value; }
+        [DisplayName("End Date")]
+        public int EndDate1 { get => EndDate; set => EndDate = value; }
+        
+    }
+}
